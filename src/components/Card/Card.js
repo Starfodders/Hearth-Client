e078c7fe94c952b4d6be236bc2210270312e553b
Michael Deng <michaeldeng55@gmail.com>
2023-04-01 11:28:26 -0400

    more stylings for cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,24 @@
 import "./Card.scss"
 import ProgressBar from '../ProgressBar/ProgressBar';
+import { useState, useEffect } from "react"
 
 const Card = ({id, name, sections, available, images}) => {
+    const [ isAvailable, setIsAvailable ] = useState(false)
+
+    useEffect(() => {
+        if (available === 1) {
+            setIsAvailable(true)
+        } else {
+            setIsAvailable(false)
+        }
+    }, [available])
 
     return (
-        <div key = {id} className = "card__container">
+        <div key = {id} className = {isAvailable ? "card__container" : "card__container__off"}>
             <h2 className = "card__name">{name}</h2>
             <img src = {`http://localhost:8080${images}`} alt = {name} className = "card__image"/>
             <ProgressBar/>
-            <button>Continue</button>
+            <button className = "card__button">{isAvailable ? 'Continue' : 'Unavailable'}</button>
         </div>
     );
 };
