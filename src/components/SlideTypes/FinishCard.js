94a6e9ff5c0b48abc9e3d62cb8dbdbedf9f7b6b7
Michael Deng <michaeldeng55@gmail.com>
2023-04-08 23:24:08 -0400

    success in final page updating user progress

--- src/components/SlideTypes/FinishCard.js
+++ src/components/SlideTypes/FinishCard.js
@@ -1,12 +1,23 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "./FinishCard.scss"
 
 const FinishCard = ({details}) => {
+    const navigate = useNavigate()
+
     const {id, content, image} = details
     const userID = sessionStorage.getItem('userId')
-    console.log(content);
 
     function handleUnitComplete() {
-
+        axios.patch(`http://localhost:8080/users/update/${userID}/${id}`)
+        .then((response) => {
+            if (response) {
+                navigate(-1)
+            }
+        })
+        .catch((error) => {
+            console.log(error + ' Error updating user');
+        })
     }
 
     return (
@@ -16,7 +27,7 @@ const FinishCard = ({details}) => {
                     {image ? <img src = {image} alt = "Congratulations on finishing the unit"/> : null }
                     </div> : null }
                 <h3 className="finish__message">{content}</h3>
-                <button className="finish__confirm">Complete</button>
+                <button className="finish__confirm" onClick ={() => handleUnitComplete()}>Complete</button>
             </div>
         </div>
     );
