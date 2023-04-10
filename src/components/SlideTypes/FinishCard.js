eaf07797359f6c1c252e2ee228ef8593a5d4b2d2
Michael Deng <michaeldeng55@gmail.com>
2023-04-09 20:16:09 -0400

    overflow fix, some image styling for slide finish

--- src/components/SlideTypes/FinishCard.js
+++ src/components/SlideTypes/FinishCard.js
@@ -1,36 +1,54 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./FinishCard.scss"
+import "./FinishCard.scss";
 
-const FinishCard = ({details}) => {
-    const navigate = useNavigate()
+const FinishCard = ({ details }) => {
+  const navigate = useNavigate();
 
-    const {id, content, image} = details
-    const userID = sessionStorage.getItem('userId')
+  const { id, content, image, caption } = details;
+  console.log(image);
+  const userID = sessionStorage.getItem("userId");
 
-    function handleUnitComplete() {
-        axios.patch(`http://localhost:8080/users/update/${userID}/${id}`)
-        .then((response) => {
-            if (response) {
-                navigate(-1)
-            }
-        })
-        .catch((error) => {
-            console.log(error + ' Error updating user');
-        })
-    }
+  function handleUnitComplete() {
+    axios
+      .patch(`http://localhost:8080/users/update/${userID}/${id}`)
+      .then((response) => {
+        if (response) {
+          navigate(-1);
+        }
+      })
+      .catch((error) => {
+        console.log(error + " Error updating user");
+      });
+  }
 
-    return (
-        <div className='finish__wrapper'>
-            <div className="finish__container">
-                {image ? <div className="finish__image">
-                    {image ? <img src = {image} alt = "Congratulations on finishing the unit"/> : null }
-                    </div> : null }
-                <h3 className="finish__message">{content}</h3>
-                <button className="finish__confirm" onClick ={() => handleUnitComplete()}>Complete</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="finish__wrapper">
+      <div className="finish__container">
+        <h3 className="finish__message">{content}</h3>
+        {image ? (
+          <div className="finish__image">
+            {image ? (
+              <>
+                <img
+                  src={`http://localhost:8080/${image}`}
+                  alt="Congratulations on finishing the unit"
+                  className="finish__image--el"
+                />
+                <p className="finish__caption">{caption}</p>
+              </>
+            ) : null}
+          </div>
+        ) : null}
+        <button
+          className="finish__confirm"
+          onClick={() => handleUnitComplete()}
+        >
+          Complete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FinishCard;
