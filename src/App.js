ac96476103d4084f5b0cb979fe2f270bba9f1a2d
Michael Deng <michaeldeng55@gmail.com>
2023-05-03 11:52:43 -0400

    auto capitalize given name if lowercase

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [displayName, setDisplayName] = useState(sessionStorage.getItem('currentName'));
 
+  function capitalizeName() {
+    return displayName.charAt(0).toUpperCase() + displayName.substring(1, displayName.length);
+  }
+
   return (
     <BrowserRouter>
       <Routes>
@@ -35,8 +39,8 @@ function App() {
           path="/home"
           element={
             <>
-              <TopLogo name={displayName} login = {setIsLoggedIn} />
-              <HomePage isLoggedIn={isLoggedIn} name = {displayName}/>
+              <TopLogo name={capitalizeName()} login = {setIsLoggedIn} />
+              <HomePage isLoggedIn={isLoggedIn} name = {capitalizeName()}/>
             </>
           }
         ></Route>
