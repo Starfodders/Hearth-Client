4652e505130c4b5f287332a87ad32f3d269cc152
Michael Deng <michaeldeng55@gmail.com>
2023-06-21 19:45:52 -0400

    missed a variable

--- src/components/GoNextContent/GoNextContent.js
+++ src/components/GoNextContent/GoNextContent.js
@@ -17,7 +17,7 @@ const GoNextContent = () => {
 
   function navigateToUnit() {
     //   axios.get(`http://localhost:8080/units/${navigateUnit}/all`)
-      axios.get(`/.netlify/functions/units/list?currUnit=${currUserNavigateUnit}`)
+      axios.get(`/.netlify/functions/units/list?currUnit=${navigateUnit}`)
         .then((response) => {
           const modifyUnitName = response.data[0].name.replace(" ", "%20");
           navigate(`/unit/${modifyUnitName}/${navigateUnit}`);
