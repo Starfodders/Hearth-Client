fffb9a4d6166d8aedda0baa0420d44dc0ff4aeb3
Michael Deng <michaeldeng55@gmail.com>
2023-09-03 14:40:21 -0400

    new netlify fx and adjust endpoints for prod

--- src/components/GuestSignUpNew/GuestSIgnUpNew.js
+++ src/components/GuestSignUpNew/GuestSIgnUpNew.js
@@ -37,16 +37,14 @@ const GuestSignUpNew = ({
     localStorage.setItem("guest-profile-id", generateGuestUUID());
     localStorage.setItem("guest-profile-pw", generateGuestPW());
 
-    axios
-      // .post("http://localhost:8080/users/signup", {
+    // axios.post("http://localhost:8080/users/signup", {
         axios.post("/.netlify/functions/user/signup", {
         given_name: localStorage.getItem("guest-profile-name"),
         email: localStorage.getItem("guest-profile-id"),
         password: localStorage.getItem("guest-profile-pw"),
       })
       .then((response) => {
-        axios
-          // .post("http://localhost:8080/users/login", {
+        // axios.post("http://localhost:8080/users/login", {
             axios.post(`/.netlify/functions/user/login`, {
             email: localStorage.getItem("guest-profile-id"),
             password: localStorage.getItem("guest-profile-pw"),
